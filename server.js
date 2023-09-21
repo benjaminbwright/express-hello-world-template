cfc608a45c8729ca479021c2fa12f4bb373e3482
Ben Wright <bwright@2u.com>
2023-09-21 13:49:43 -0500

    Adds home route

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // ROUTES
+app.get("/", (req, res) => res.send("It's the home page."));
 
 // START THE SERVER
 app.listen(PORT, () =>
